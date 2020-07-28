0e53879cc4bb0d33dfc25a89b245d1eeda8ead5e
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-28 01:20:28 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,14 @@ class String
   end
 
   def count_sentences
+<<<<<<< HEAD
     array = self.split /\b\.+|!+|\?/
       # This regex needed a "one or more" application to the "." and "!" option - otherwise, it returned an extra string.
     array.length
+=======
+    array = self.split /\b\.+|!|\?/
+    array.length
+    #binding.pry
+>>>>>>> a01ce158f48bc394b31ab366b30d1ad81550dca2
   end
 end
